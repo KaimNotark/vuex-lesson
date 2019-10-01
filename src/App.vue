cf32ed7196b91a7430181ab3bf0278812020ddab
KaimNotark <KaimNotark@yandex.ru>
2019-10-02 02:50:45 +0800

    initial project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Vuex</h1>
+    <app-result :counter="counter"></app-result>
+    <app-counter @updated="counter += $event"></app-counter>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+import Result from "./components/Result.vue";
+
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  components: {
+    appCounter: Counter,
+    appResult: Result
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button class="btn" @click="increment">Increment</button>
+    <button class="btn" @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$emit("updated", 1);
+    },
+    decrement() {
+      this.$emit("updated", -1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+  margin-left: 10px;
+}
+</style>
