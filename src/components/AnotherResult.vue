dc0fdc56a22c3dd6172397cca4e9aef293e6ede1
KaimNotark <KaimNotark@yandex.ru>
2019-10-04 00:31:57 +0800

    add mapGetters in AnotherCounter.vue

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,13 +1,16 @@
 <template>
-  <p>Another Counter is: {{ counter }}</p>
+  <div>
+    <p>Another Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }}</p>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   computed: {
-    counter() {
-      return this.$store.getters.doubleCounter;
-    }
+    ...mapGetters(["doubleCounter", "stringCounter"])
   }
 };
 </script>
