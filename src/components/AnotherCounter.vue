f55a879f48dd9228bc6d6784b16876d58c8f505c
KaimNotark <KaimNotark@yandex.ru>
2019-10-04 02:24:39 +0800

    add mutation

---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button class="btn" @click="increment">Increment</button>
+    <button class="btn" @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+
+export default {
+  methods: {
+    ...mapMutations(["increment", "decrement"])
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+  margin-left: 10px;
+}
+</style>
